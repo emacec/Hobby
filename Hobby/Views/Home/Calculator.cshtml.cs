7f596fd99029015d114012b5e5f91589c6a95d35
emacec <emacec@byu.edu>
2025-02-07 20:57:08 -0700

    Add project files.

---
+++ Hobby/Views/Home/Calculator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hobby.Views.Home
+{
+    public class CalculatorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
